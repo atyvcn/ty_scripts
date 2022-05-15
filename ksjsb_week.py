3c295834afc569b945fbefd5bf38648cdf58f0b9
TY <nyqty@qq.com>
2022-05-15 20:29:44 +0800

    Update ksjsb_week.py

--- ksjsb_week.py
+++ ksjsb_week.py
@@ -4,7 +4,7 @@
 # @Author  : HarbourJ,TengYu
 # @TG      : https://t.me/HarbourToulu
 # @File    : ksjsb_week.py
-# @Description: kuaishou_api_sts.txt为你快手极速版ck存放文件，一行一个ck；
+# @Description: 变量ksCookie
 #               code为你的助力码（获取方式：快手极速版 ->周周赚金币->分享保存二维码-二维码转链接-把短链放到浏览器访问转为长链-最后一组数字即为你的助力码）
 """
 cron: 20 13 * * *
@@ -21,7 +21,6 @@ except Exception as e:
 
 urllib3.disable_warnings()
 
-
 def generate_random_did(randomlength=16):
     """
     生成一个指定长度的随机字符串
@@ -36,8 +35,8 @@ def generate_random_did(randomlength=16):
 def ksjsbFriendAssist(api_st, code):
     url = "https://nebula.kuaishou.com/rest/zt/encourage/assistance/friendAssist"
     payload = "{\"assistanceId\":\"" + code + "\"}"
-    if 'did=' in api_st:
-        api_st = 'did=ANDROID_' + generate_random_did(16) + '; '
+    if 'did=' not in api_st:
+        api_st = 'did=ANDROID_' + generate_random_did(16) + '; ' + api_st
     if 'kpf=' not in api_st:
         api_st = 'kpf=ANDROID_PHONE; ' + api_st
     if 'kpn=' not in api_st:
@@ -62,9 +61,10 @@ def ksjsbFriendAssist(api_st, code):
 if __name__ == '__main__':
 
     code = "2771506497757541"
+    #shareObjectId=2771662732694180
 
     api_stCK = []
-
+    
     if "ksCookie" in os.environ:
         api_stCK = os.environ["ksCookie"].split("&")
         print("共计%s个快手api_st" % len(api_stCK))
